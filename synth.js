2d22e3616c24728eed38c897beb10c01ebe5f2c3
A'Jahn Kimble <ajahnkimble@gmail.com>
2020-01-24 19:11:02 -0500

    attempted to make a synth from scratch

--- synth.js
+++ synth.js
@@ -1,33 +1,48 @@
-console.clear();
-
-//Start Audio Context
 
 
+function startAudioContext(){    
+    document.querySelector('#play-button').addEventListener('click', async () =>{
+            await Tone.start()
 
-document.querySelector('#play-button').addEventListener('click',function(){
-    
-    
-    if (Tone.context.state !== 'running'){
-        Tone.context.resume();
-        console.log("context running");
-    }
-   
-})
+            let synth = new Tone.FMSynth().toMaster();
+            synth.triggerAttackRelease("c5", "8n");
+    })
+}
+
+startAudioContext();
+
 
 //Oscillators
+
+    //Setup volume env
+    let gainNode = Tone.context.createGain();
+    let env = new Tone.Envelope().connect(gainNode.gain);
+    //Setup oscs
+    let osc1 = new Tone.Oscillator(440,"square")connect(env).start();
+//    let osc2 = new Tone.Oscillator(440,"square");
+//    let osc3 = new Tone.Oscillator(440,"square");
+    triggerSynth(env);
+
+
+
 //control relative pitch of oscillators
 //button to change waveshape
 //connect to filter
 
 //buttons trigger synth
-let key = document.querySelector('#c\\#').addEventListener("click",clickKeys);
 
-function clickKeys(){
+ 
+    
+let key = document.querySelector("#c").addEventListener("click",()=>triggerSynth(env));
+
+
+
+function triggerSynth(env){
     console.log("key clicked!");
-    const synth = new Tone.MetalSynth().toMaster();
-
-    synth.triggerAttackRelease("C4", "4n");
+    
+   env.triggerAttackRelease('4n','4n');
 }
+
 //buttons change abs pitch of oscillators
 //sixteen keys control abs pitch of osccillators
 
