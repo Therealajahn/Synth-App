3fa2eef823be036b3b01b5d0325449a00f7f9641
A'Jahn Kimble <ajahnkimble@gmail.com>
2020-01-27 09:44:20 -1200

    got click keyboard working

--- synth.js
+++ synth.js
@@ -3,48 +3,70 @@
 function startAudioContext(){    
     document.querySelector('#play-button').addEventListener('click', async () =>{
             await Tone.start()
-
-            let synth = new Tone.FMSynth().toMaster();
-            synth.triggerAttackRelease("c5", "8n");
     })
 }
 
 startAudioContext();
 
+//setup Sound
+//Setup volume
+    let gain = new Tone.Gain(0.1).toMaster();
+//Setup env   
+    let env = new Tone.AmplitudeEnvelope({
+            "attack" : 0.01,
+			"decay" : 0.1,
+			"sustain" : 0.5,
+			"release" : 0.5
+        }).connect(gain);
+   
+//Setup oscs
+   
+    let osc1 = new Tone.Oscillator(440,"square").connect(env).start();
+    let osc2 = new Tone.Oscillator(440, "square").connect(env).start();
+    let osc3 = new Tone.Oscillator(440, "square").connect(env).start();
 
-//Oscillators
+let sound = {
+    osc1: osc1,
+    osc2: osc2,
+    osc3: osc3,
+    env: env
+}
 
-    //Setup volume env
-    let gainNode = Tone.context.createGain();
-    let env = new Tone.Envelope().connect(gainNode.gain);
-    //Setup oscs
-    let osc1 = new Tone.Oscillator(440,"square")connect(env).start();
-//    let osc2 = new Tone.Oscillator(440,"square");
-//    let osc3 = new Tone.Oscillator(440,"square");
-    triggerSynth(env);
+
+//buttons trigger synth
+
+    
+document.addEventListener("mousedown",()=>{
+        if(event.target.className.baseVal === "key"){
+       triggerSynth(1,sound,event.target.id)
+        }
+    });
+
+document.addEventListener("mouseup",()=>triggerSynth(0,env));
+
+document.addEventListener("keydown",()=>triggerSynth(1,sound,keyboard,event.key))
+
+function triggerSynth(gate,sound,note){
+    const { osc1, osc2, osc3 } = sound;
+    if(gate){
+        
+        osc1.frequency.value = note;
+        osc2.frequency.value = osc1.frequency.value + 1;
+        osc3.frequency.value = osc1.frequency.value - 1;
+        
+        env.triggerAttack();
+        
+        
+    }else{
+        env.triggerRelease();
+    }
+}
 
 
 
 //control relative pitch of oscillators
 //button to change waveshape
 //connect to filter
-
-//buttons trigger synth
-
- 
-    
-let key = document.querySelector("#c").addEventListener("click",()=>triggerSynth(env));
-
-
-
-function triggerSynth(env){
-    console.log("key clicked!");
-    
-   env.triggerAttackRelease('4n','4n');
-}
-
-//buttons change abs pitch of oscillators
-//sixteen keys control abs pitch of osccillators
 
 
 
