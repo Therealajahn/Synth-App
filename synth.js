40e110211e654bcef0412d97d59b606c715cb63d
A'Jahn Kimble <ajahnkimble@gmail.com>
2020-01-27 16:25:58 -1200

    Volca bass now stored in a js file; notes can now be played with the keyboard

--- synth.js
+++ synth.js
@@ -5,7 +5,7 @@
 //            await Tone.start()
 //    })
 //}
-
+//
 //startAudioContext();
 
 //setup Sound
@@ -15,8 +15,8 @@
     let env = new Tone.AmplitudeEnvelope({
             "attack" : 0.01,
 			"decay" : 0.1,
-			"sustain" : 0.5,
-			"release" : 0.5
+			"sustain" : 1,
+			"release" : 5
         }).connect(gain);
    
 //Setup oscs
@@ -38,30 +38,74 @@ const sound = {
     
 document.addEventListener("mousedown",()=>{
         if(event.target.className.baseVal === "key"){
-       triggerSynth(1,sound,event.target.id)
+       buttonTriggerSynth(1,sound,event.target.id)
         }
     });
 
-document.addEventListener("mouseup",()=>triggerSynth(0,env));
+document.addEventListener("mouseup",()=>buttonTriggerSynth(0,sound));
 
-document.addEventListener("keydown",()=>triggerSynth(1,sound,keyboard,event.key))
 
-function triggerSynth(gate,sound,note){
-    const { osc1, osc2, osc3 } = sound;
+
+function buttonTriggerSynth(gate,sound,note){
+    const { osc1, osc2, osc3, env } = sound;
     if(gate){
         
         osc1.frequency.value = note;
-        osc2.frequency.value = osc1.frequency.value + 1;
-        osc3.frequency.value = osc1.frequency.value - 1;
+        osc2.frequency.value = osc1.frequency.value + 5;
+        osc3.frequency.value = osc1.frequency.value - 5;
         
         env.triggerAttack();
         
         
-    }else{
+    }else if (!gate){
         env.triggerRelease();
     }
 }
 
+//Define keys to use for keyboard
+const keyboard = {
+    //bottom row
+    z: "c1",
+    s: "c#1",
+    x: "d1",
+    d: "d#1",
+    c: "e1",
+    v: "f1",
+    g: "f#1",
+    b: "g1",
+    h: "g#1",
+    n: "a1",
+    j: "a#1",
+    m: "b1",
+    ',': "c2",
+    l: "c#2",
+    '.': "d2",
+    ';': "d#2",
+    '/': "e2",
+}
+
+
+document.addEventListener("keydown",()=>keyTriggerSynth(1,sound,keyboard,event.key))
+
+document.addEventListener("keyup",()=>keyTriggerSynth(0,sound,keyboard,event.key))
+
+function keyTriggerSynth(gate,sound,keyboard,key){
+    console.log(key);
+    const { osc1, osc2, osc3, env } = sound;
+    
+    if(gate && keyboard[key]){
+        
+        osc1.frequency.value = keyboard[key];
+        osc2.frequency.value = osc1.frequency.value  + 50 ;
+        osc3.frequency.value = osc1.frequency.value + 100;
+        
+        env.triggerAttack();
+        
+        
+    }else if (!gate){
+        env.triggerRelease();
+    }
+}
 
 
 //control relative pitch of oscillators
